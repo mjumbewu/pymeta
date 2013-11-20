23528c9ed98041ab67600c20a880a19776487df8
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-11-20 03:48:46 -0500

    Use the Markdown README in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.5.0",
     url="http://launchpad.net/pymeta",
     description="Pattern-matching language based on OMeta",
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     author="Allen Short",
     author_email="washort42@gmail.com",
     license="MIT License",
